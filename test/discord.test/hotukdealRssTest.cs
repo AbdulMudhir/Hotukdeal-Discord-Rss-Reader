80277a03d69fdb0ea4e5542df084ed8cdfe1d3b7
abdul mudhir <abdulmudhirss@gmail.com>
2020-06-20 22:21:24 +0100

    unit test for deals coming through rss

--- test/discord.test/hotukdealRssTest.cs
+++ test/discord.test/hotukdealRssTest.cs
@@ -9,15 +9,19 @@ namespace discord.Test
     public class HotUKDealRssTest
     {
 
+         HotUkDealRssReader hotUKDealRss = new HotUkDealRssReader();
+
+      
         [Fact]
         public void RssFeedCount()
         {
             
-            
-            HotUkDealRssReader hotUKDealRss = new HotUkDealRssReader();
+                  Hotukdeals hotukdeals = hotUKDealRss.hotukdeals();
 
-            Hotukdeals hotukdeals = hotUKDealRss.hotukdeals();
 
+           
+
+          
             int totalDeal =  hotukdeals.Total;
 
             // should recieve 20 feed from RSS
@@ -25,6 +29,22 @@ namespace discord.Test
 
 
             
+        }
+
+        [Fact]
+
+        public void RssFeedItem()
+        {
+            Hotukdeals hotukdeals = hotUKDealRss.hotukdeals();
+            Deal deal = hotukdeals.deals[0];
+
+            String firstItemName = deal.Name;
+
+            string link = deal.Link;
+
+            Assert.Equal("1,000 free audiobooks @ Open Culture",firstItemName);
+            Assert.Equal("https://www.hotukdeals.com/deals/1000-free-audiobooks-3490202", link);
+
         }
 
 
