c173deccab2709a024a495989e9281078190ac8b
abdul mudhir <abdulmudhirss@gmail.com>
2020-06-21 18:30:55 +0100

    hotukdeal scrapped created

--- src/discord/HotukdealScraper.cs
+++ src/discord/HotukdealScraper.cs
@@ -36,7 +36,6 @@ namespace Hotukdeal
                     var titleNode = deal.SelectSingleNode(".//a[@class ='cept-tt thread-link linkPlain thread-title--list' ]");
                     var title = HttpUtility.HtmlDecode(titleNode.InnerText);
 
-
                     var link = titleNode.Attributes["href"].Value;
 
                     // by pass the lazy load images
@@ -45,10 +44,24 @@ namespace Hotukdeal
 
                     var price = deal.SelectSingleNode(".//span[@class= 'thread-price text--b vAlign--all-tt cept-tp size--all-l size--fromW3-xl']").InnerText;
 
-
-                    string description = deal.SelectSingleNode(".//div[@class='cept-description-container overflow--wrap-break width--all-12  size--all-s size--fromW3-m']").InnerText;
+                    var description = deal.SelectSingleNode(".//div[@class='cept-description-container overflow--wrap-break width--all-12  size--all-s size--fromW3-m']").InnerText;
                     description = description.Replace("Read more", "");
-                    System.Console.WriteLine(description);
+
+                    
+                    var hotMeters = hotMeter(deal);
+
+                    var merchantName = deal.SelectSingleNode(".//span[@class='cept-merchant-name text--b text--color-brandPrimary link']").InnerText;
+
+                    System.Console.WriteLine(merchantName);
+
+                    var directLink = Parser.getDirectLink(link);
+
+                    var commentCounts = commentCount(deal, link);
+
+                    var madeHotDate = deal.SelectSingleNode(".//span[@class ='hide--toW3']").InnerText;
+
+                   
+
 
                     }
 
@@ -63,13 +76,37 @@ namespace Hotukdeal
 
         }
 
+        
+        private string commentCount(HtmlNode deal, string url)
+        {   // get the parent comment node then get the inner childs and retreive their value
+            var commentNode = deal.SelectSingleNode($".//a[@class  = 'cept-comment-link btn space--h-3 btn--mode-boxSec cept-comment-link-id-{Parser.forumNumber(url)} ']");
 
-        public Boolean validDeal(HtmlNode deal)
+            return commentNode.ChildNodes[1].InnerText;;
+        }
+
+        private string hotMeter(HtmlNode deal)
+        
+        {      
+            string meter;
+
+            try{
+                    meter = deal.SelectSingleNode(".//span[@class='cept-vote-temp vote-temp vote-temp--hot']").InnerText;
+                }
+                catch(NullReferenceException)
+                { 
+                    meter = deal.SelectSingleNode(".//span[@class='cept-vote-temp vote-temp vote-temp--burn']").InnerText;
+
+                }
+
+            return meter.Trim();
+        }
+
+        private Boolean validDeal(HtmlNode deal)
         {
             return deal.SelectSingleNode(".//a[@class ='cept-tt thread-link linkPlain thread-title--list' ]") != null;
         }
 
-        public Boolean expiredDeal(HtmlNode deal)
+        private Boolean expiredDeal(HtmlNode deal)
         {
          
 
